3cf4b2ff815f0723a14f873a2eaf1f653160b791
JSQdev <148073798+devJSQ@users.noreply.github.com>
2023-12-10 19:08:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-          /* try (Scanner object)) { 
-                your code here 
-           } */
+          try (Scanner scanner = new Scanner (System.in)){
+            System.out.print("Enter your name: ");
+            String name = scanner.next();
+            System.out.println("Hello " + name);
+
+          }
     }
 }
